411519d8f74912a32b1ae946b8ece183e87fa7de
Max Khrapatiy <max.khrapatiy@gmail.com>
2023-12-24 01:00:46 +0300

    Correction of a typo.

--- src/main/java/org/example/initializer/PostInitializer.java
+++ src/main/java/org/example/initializer/PostInitializer.java
@@ -7,7 +7,6 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
-import java.util.Arrays;
 
 public class PostInitializer implements WebApplicationInitializer {
     private static final Logger myLogger = LogManager.getLogger(PostInitializer.class);
@@ -17,7 +16,6 @@ public class PostInitializer implements WebApplicationInitializer {
         final var context = new AnnotationConfigWebApplicationContext();
         context.scan("org.example");
         context.refresh();
-        System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
 
         final var servlet = new DispatcherServlet(context);
         final var registration = servletContext.addServlet("app", servlet);
